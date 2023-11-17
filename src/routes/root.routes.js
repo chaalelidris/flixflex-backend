19c83327c5223f408367df77d4f13b98047a353f
idris chaalel <idrischaalel8@gmail.com>
2023-11-17 23:46:48 +0100

    update package

--- src/routes/root.routes.js
+++ src/routes/root.routes.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import passport from 'passport';
 /* controllers */
 import getRoot from '../controllers/root/getRoot.js';
 import postRoot from '../controllers/root/postRoot.js';
 
 const root = express.Router()
 
-root.get('/', passport.authenticate('jwt', { session: false }), getRoot)
+root.get('/', getRoot)
 root.post('/', postRoot)
 
 export default root
