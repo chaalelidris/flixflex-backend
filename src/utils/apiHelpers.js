b6c4a138e52a14f4e55cc5877dee31ad6ea3e8d6
idris chaalel <idrischaalel8@gmail.com>
2023-11-21 13:36:42 +0100

    feat: update error handling

--- src/utils/apiHelpers.js
+++ src/utils/apiHelpers.js
@@ -2,7 +2,7 @@
 const handleApiError = (res, controllerName, error) => {
     console.error(`Error in ${controllerName}: ${error}`);
     if (error.response) {
-        res.status(error.response.status).json({ success: false, error: error.response.data.status_message });
+        res.status(error.response.status).json({ success: false, error: error.response.data.status_message ?? "Error" });
     } else {
         res.status(500).json({ success: false, error: 'Internal server error' });
     }
